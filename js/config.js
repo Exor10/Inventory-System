999ab0efe86eefd99a225c097983f4b3757698dd
Nathan Comiso <84970206+Exor10@users.noreply.github.com>
2026-05-25 15:03:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycby9tl0H6Yg1U2_egO3KR4x_S-8wsH2C_s-oSvZT4tYc2dqRDmhc_ZiaJuq-31bk-2Bs/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbylVDMUkO5DduveRNuoVnpVjltayJ9oE_hfx3Mx0gNXTLHs0mLLIRkAEaYdZhT8QIaI/exec',
 
   SCAN_COOLDOWN_MS:   1200,   // minimum ms between accepted scans
   DASHBOARD_REFRESH:  30000,  // ms between dashboard auto-refreshes
